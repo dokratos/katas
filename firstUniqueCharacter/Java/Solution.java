12e6ce9d6a550d93cd6f4d8683fa24d057d2bd7f
dokratos <dokratos@hotmail.it>
2024-02-05 12:39:32 +0100

    stream solution

---
+++ firstUniqueCharacter/Java/Solution.java
@@ -0,0 +1,34 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+class Solution {
+     public static void main(String[] strings) {
+         new Solution().firstUniqChar("leetcode");
+     }
+
+
+    public int firstUniqChar(String s) {
+        int index = -1;
+        Map<String, Integer> counting = Stream.of(s.split(""))
+                .collect(Collectors.toMap(
+                        x -> x,
+                        String::length,
+                        Integer::sum)
+                );
+        System.out.println(counting.get("e"));
+
+            for (int i = 0; i < s.length(); i++) {
+                System.out.println(s.charAt(i));
+
+                 int value = counting.get(String.valueOf(s.charAt(i)));
+
+                if (value == 1) {
+                    return i;
+                }
+            }
+
+        return index;
+    }
+}
