ceb93a9826d73b1ccd034ceebbfa9af39d19eaf7
dokratos <dokratos@hotmail.it>
2024-02-06 19:20:19 +0100

    try implement - time limit exceeded

--- minWindowSubString/Java/Solution.java
+++ minWindowSubString/Java/Solution.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -12,7 +13,11 @@ class Solution {
         }
 
         if (tLen == sLen) {
-            if (s.equals(t)) return s;
+            char[] sChar = s.toCharArray();
+            Arrays.sort(sChar);
+            char[] tChar = t.toCharArray();
+            Arrays.sort(tChar);
+            if (new String(sChar).equals(new String(tChar))) return s;
             return "";
         }
 
@@ -67,20 +72,84 @@ class Solution {
                 if (sub.containsKey(val.getKey()) && sub.get(val.getKey()) == val.getValue()) {
                     isContained = true;
                 } else {
-                    System.out.println(sub.toString() + "second loop" + isContained);
+                    // System.out.println(sub.toString() + "second loop" + isContained);
                     isContained = false;
                     break;
                 }
             }
 
             if (!isContained) {
-                secondIndex = j - 1;
+                secondIndex = j -1;
                 // System.out.println(secondIndex + " index 2");
                 break;
             }
         }
 
+        int thirdPointer = firstIndex;
+        int fourthPointer = secondIndex;
 
-        return s.substring(secondIndex, firstIndex);
+        for (int i = firstIndex + 1, j = secondIndex; i < sLen;) {
+            Map<String, Integer> sub = Stream.of(s.substring(j, i).split(""))
+                    .collect(Collectors.toMap(
+                            x -> x,
+                            String::length,
+                            Integer::sum));
+
+            for (Map.Entry<String, Integer> val : myMap.entrySet()) {
+                if (sub.containsKey(val.getKey()) && sub.get(val.getKey()) == val.getValue()) {
+                    isContained = true;
+                } else {
+                    isContained = false;
+                    i++;
+                    thirdPointer = i;
+                    break;
+                }
+            }
+
+            if (isContained) {
+                j++;
+                fourthPointer = j;
+                continue;
+            }
+            i++;
+            thirdPointer = i;
+
+            if (i == sLen) {
+                while (i - j > tLen) {
+                    j++;
+                    Map<String, Integer> sub2 = Stream.of(s.substring(j, i).split(""))
+                            .collect(Collectors.toMap(
+                                    x -> x,
+                                    String::length,
+                                    Integer::sum));
+                    for (Map.Entry<String, Integer> val : myMap.entrySet()) {
+                        if (!sub2.containsKey(val.getKey()) || sub2.get(val.getKey()) != val.getValue()) {
+                            break;
+                        }
+                    }
+                    fourthPointer = j-1;
+                }
+            }
+        }
+
+        // if (thirdPointer == sLen) {
+        //         while (thirdPointer - fourthPointer > tLen) {
+        //             int j = fourthPointer;
+        //             j++;
+        //             Map<String, Integer> sub2 = Stream.of(s.substring(j, thirdPointer).split(""))
+        //                 .collect(Collectors.toMap(
+        //                     x -> x,
+        //                     String::length,
+        //                     Integer::sum));
+        //             for (Map.Entry<String, Integer> val : myMap.entrySet()) {
+        //                 if (!sub2.containsKey(val.getKey()) || sub2.get(val.getKey()) != val.getValue()) {
+        //                     break;
+        //                 }
+        //             }
+        //             fourthPointer = j-1;
+        //         }
+        //     }
+
+        return firstIndex - secondIndex < thirdPointer - fourthPointer ? s.substring(secondIndex, firstIndex) : s.substring(fourthPointer, thirdPointer);
     }
 }
