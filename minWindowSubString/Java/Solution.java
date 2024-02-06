d9855aebb027fb275603d73ccf982b651b0c5eb0
dokratos <dokratos@hotmail.it>
2024-02-06 17:45:38 +0100

    pass a few test, missing last sliding loop

--- minWindowSubString/Java/Solution.java
+++ minWindowSubString/Java/Solution.java
@@ -7,7 +7,14 @@ class Solution {
         int sLen = s.length();
         int tLen = t.length();
 
-        if (tLen > sLen) return "";
+        if (tLen > sLen) {
+            return "";
+        }
+
+        if (tLen == sLen) {
+            if (s.equals(t)) return s;
+            return "";
+        }
 
         Map<String, Integer> myMap = Stream.of(t.split(""))
                 .collect(Collectors.toMap(
@@ -16,16 +23,15 @@ class Solution {
                         Integer::sum));
 
         int firstIndex = 0;
-        int secondIndex = sLen;
+        int secondIndex = 0;
         boolean isContained = false;
 
-        for (int j = tLen; j < sLen; j++) {
+        for (int j = tLen; j <= sLen; j++) {
             Map<String, Integer> sub = Stream.of(s.substring(0, j).split(""))
                     .collect(Collectors.toMap(
                             x -> x,
                             String::length,
                             Integer::sum));
-
             for (Map.Entry<String, Integer> val : myMap.entrySet()) {
                 if (sub.containsKey(val.getKey()) && sub.get(val.getKey()) == val.getValue()) {
                     isContained = true;
@@ -34,37 +40,47 @@ class Solution {
                     break;
                 }
             }
-
+            // System.out.println("after loop " + isContained);
             if (isContained) {
                 firstIndex = j;
                 break;
             }
         }
 
-        if (!isContained) return "";
+        if (!isContained) {
+            // System.out.println("fhhhh" + isContained);
+            return "";
+        }
 
-        for (int j = sLen - tLen; j > 0; j--) {
-            Map<String, Integer> sub = Stream.of(s.substring(j).split(""))
+
+        for (int j = 0; j <= firstIndex; j++) {
+            // System.out.println("in second loop " + firstIndex);
+            Map<String, Integer> sub = Stream.of(s.substring(j, firstIndex).split(""))
                     .collect(Collectors.toMap(
                             x -> x,
                             String::length,
                             Integer::sum));
 
+
             for (Map.Entry<String, Integer> val : myMap.entrySet()) {
-                if (sub.containsKey(val.keySet()) && sub.get(val.keySet()) == val.values()) {
+                // System.out.println(val.getKey() + " inner " + sub.get(val.getKey()) + "   " + isContained + val.getValue());
+                if (sub.containsKey(val.getKey()) && sub.get(val.getKey()) == val.getValue()) {
                     isContained = true;
                 } else {
+                    System.out.println(sub.toString() + "second loop" + isContained);
                     isContained = false;
                     break;
                 }
             }
 
-            if (isContained) {
-                secondIndex = j;
+            if (!isContained) {
+                secondIndex = j - 1;
+                // System.out.println(secondIndex + " index 2");
                 break;
             }
         }
 
-        return sLen - secondIndex > firstIndex ? s.substring(0, firstIndex) : s.substring(secondIndex);
+
+        return s.substring(secondIndex, firstIndex);
     }
 }
