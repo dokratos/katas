134f2a1af6971f99396539cbedac8d3a48553daa
dokratos <dokratos@hotmail.it>
2024-02-06 13:14:50 +0100

    clean solution

--- groupAnagrams/Java/Solution.java
+++ groupAnagrams/Java/Solution.java
@@ -7,11 +7,8 @@ import java.util.stream.Stream;
 
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> result = new ArrayList<>();
-
         if (strs.length == 1) {
-            result.add(List.of(strs[0]));
-            return result;
+            return List.of(List.of(strs[0]));
         }
 
         Map<Map<String, Integer>, List<String>> hashKeys = new HashMap<>();
@@ -25,31 +22,6 @@ class Solution {
             hashKeys.computeIfAbsent(sub, k -> new ArrayList<>()).add(str);
         }
 
-        for (Map.Entry<Map<String, Integer>, List<String>> hashKey : hashKeys.entrySet()) {
-            result.add(hashKey.getValue());
-        }
-//            hashKeys.add(sub);
-//        }
-//
-//        hashKeys = hashKeys.stream()
-//                .distinct()
-//                .collect(Collectors.toList());
-//
-//        for (Map<String, Integer> hashkey: hashKeys) {
-//            List<String> group = new ArrayList<>();
-//            for (String str : strs) {
-//                Map<String, Integer> sub = Stream.of(str.split(""))
-//                        .collect(Collectors.toMap(
-//                                x -> x,
-//                                String::length,
-//                                Integer::sum));
-//                if (hashkey.equals(sub)) {
-//                    group.add(str);
-//                }
-//            }
-//            result.add(group);
-//        }
-
-        return result;
+        return new ArrayList(hashKeys.values());
     }
 }
