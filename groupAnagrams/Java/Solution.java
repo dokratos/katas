3010d330f5f83b407b83f830e31c801025b817d5
dokratos <dokratos@hotmail.it>
2024-02-06 13:29:39 +0100

    more efficient solution with no hashmapping and no fast fail

--- groupAnagrams/Java/Solution.java
+++ groupAnagrams/Java/Solution.java
@@ -1,24 +1,16 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        if (strs.length == 1) {
-            return List.of(List.of(strs[0]));
-        }
-
-        Map<Map<String, Integer>, List<String>> hashKeys = new HashMap<>();
+        Map<String, List<String>> hashKeys = new HashMap<>();
 
         for (String str : strs) {
-            Map<String, Integer> sub = Stream.of(str.split(""))
-                    .collect(Collectors.toMap(
-                            x -> x,
-                            String::length,
-                            Integer::sum));
+            char[] key = str.toCharArray();
+            Arrays.sort(key);
+            String sub = new String(key);
+
             hashKeys.computeIfAbsent(sub, k -> new ArrayList<>()).add(str);
         }
 
