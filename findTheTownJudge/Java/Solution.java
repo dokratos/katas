98cd7ec9da66b3602e99dffccedf6f7c40b87588
dokratos <dokratos@hotmail.it>
2024-02-22 16:54:47 +0100

    try to implement solution - 90/92 passing tests

---
+++ findTheTownJudge/Java/Solution.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        if (n == 1) return 1;
+        if (trust.length == 0) return -1;
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
+
+        for (int[] citizen : trust) {
+            map.computeIfAbsent(citizen[0], ArrayList::new).add(citizen[1]);
+        }
+        System.out.println(map.toString());
+        Set<Integer> notJudge = map.keySet();
+        List<List<Integer>> trusted = new ArrayList<>(map.values());
+
+        L1: for (int i = 1; i <= n; i++) {
+            if (!notJudge.contains(i)) {
+                System.out.println(i);
+
+                for (List<Integer> t : trusted) {
+                    System.out.println(t.toString());
+                    if (!t.contains(i)) {
+                        continue L1;
+                    }
+                }
+                System.out.println("hi");
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
