00b139b422cd3a17dcf660ad57afd9266d361e3d
dokratos <dokratos@hotmail.it>
2024-03-07 15:22:15 +0100

    implement java solution - 0ms

---
+++ middelOfTheLinkedList/Java/Solution.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+class Solution {
+      public class ListNode {
+          int val;
+          ListNode next;
+          ListNode() {}
+          ListNode(int val) { this.val = val; }
+          ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+      }
+    public ListNode middleNode(ListNode head) {
+        ArrayList<ListNode> list = new ArrayList<>();
+
+        while (head != null) {
+            list.add(head);
+            head = head.next;
+        }
+
+        return list.get(list.size()/2);
+    }
+}
