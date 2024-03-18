2df1e30b4e397315500bc87955a05660d1eac701
dokratos <dokratos@hotmail.it>
2024-03-18 11:46:38 +0100

    improved solution sorting by index 1 only

--- minimumNumberOfArrows/Java/Solution.java
+++ minimumNumberOfArrows/Java/Solution.java
@@ -1,43 +1,28 @@
 import java.util.Arrays;
+import java.util.Comparator;
 
 class Solution {
     public int findMinArrowShots(int[][] points) {
         if (points.length == 1) return 1;
-        Arrays.sort(points, (a, b)-> {
-            if(a[0] == b[0]){
-                return a[1] - b[1];
-            }
-            return a[0] - b[0];
-        });
+        Arrays.sort(points, Comparator.comparingInt(p -> p[1]));
 
         int arrows = 1;
         int i = 0;
         int j = 1;
-        for (int[] num : points) {
-            for (int n : num) {
 
-                System.out.println(n);
-            }
-        }
         L1: while (j < points.length) {
             while (points[i][1] >= points[j][0]) {
                 j++;
-                System.out.println(i + " in loop 2" + j);
                 if (j >= points.length) {
                     break L1;
                 }
             }
 
             arrows++;
-            System.out.println(j + " in loop" + arrows);
             i = j;
             j++;
 
         }
-
-        // if (j < points.length && points[i][1] < points[j][0]) {
-        //     arrows++;
-        // }
 
         return arrows;
     }
