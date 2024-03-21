61ab04ec106807d567e47660aac9ac0d0bfec6c2
dokratos <dokratos@hotmail.it>
2024-03-21 14:59:46 +0100

    try implement - cycle error

---
+++ reverseLinkedList/Java/Solution.java
@@ -0,0 +1,30 @@
+import java.util.ArrayDeque;
+
+class ListNode {
+     int val;
+     ListNode next;
+     ListNode() {}
+     ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode reversed = new ListNode(0);
+        ListNode dummy = reversed;
+        ArrayDeque<ListNode> reorder = new ArrayDeque<>();
+
+        while (head != null) {
+            reorder.addFirst(head);
+            head = head.next;
+        }
+
+        while (!reorder.isEmpty()) {
+            dummy.next = reorder.poll();
+            dummy = dummy.next;
+            System.out.println(dummy.val);
+        }
+
+        return reversed.next;
+    }
+}
